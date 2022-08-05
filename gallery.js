d7dc93ea1441e37a7c8c19b647b0c408c0bcc5ff
Kervi Jhonata <kervij@gmail.com>
2022-08-05 00:53:40 -0300

    modal bug corrected and layout okay

--- gallery.js
+++ gallery.js
@@ -1,22 +1,27 @@
 let imagens = document.querySelectorAll(".galeria .galeria-imagem");
 let painel = document.querySelector(".galeria .floating-painel");
+painel.addEventListener('click', close, {capture:false});
 var imagemDoPainel = document.querySelector(".galeria .floating-painel .floating-painel-image");
-painel.addEventListener('click', close);
-close()
+let btnClose = document.querySelector(".btnClose");
+painel.style.visibility = "hidden";
 
 let x = 0;
 for(imagem of imagens){
     imagem.setAttribute('data-reference', x);
-    imagem.addEventListener('click', zoomIn);
+    imagem.addEventListener('click', open, {capture:false});
     x++
 }
 
-function zoomIn(evt) {
+function open(evt) {
+    evt.stopPropagation();
     let sourceURL = evt.target.getAttribute('src');
     imagemDoPainel.setAttribute('src', sourceURL);
     painel.style.visibility = "visible";
+    imagemDoPainel.removeEventListener('click',close)
 }
 
-function close() {
-    painel.style.visibility = "hidden";
+function close(e) {
+    if(e.target.classList == "floating-painel"){
+        painel.style.visibility = "hidden";
+    }
 }
